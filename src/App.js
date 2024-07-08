941edaa4512725bb36342f91107c6eb7cde924b1
Ehab Abdalla <121260254+ehab20011@users.noreply.github.com>
2024-07-08 17:59:16 -0400

    Fixed JSON files and Vulnerabilities

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import HomePage from './components/homepage';
 import Navbar from './components/navbar';
 import AnsysPage from './components/ansys';
@@ -9,20 +9,22 @@ import HackathonsPage from './components/hackathons';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Helmet>
-          <title>Ehab's Codeverse</title>
-        </Helmet>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/ansys" element={<AnsysPage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/Hackathons" element={<HackathonsPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <div className="App">
+          <Helmet>
+            <title>Ehab's Codeverse</title>
+          </Helmet>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/ansys" element={<AnsysPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/Hackathons" element={<HackathonsPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </HelmetProvider>
   );
 }
 
